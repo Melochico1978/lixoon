1bc33987d4867b53b5bfab508ad54547e169dec6
Melochico1978 <melochico1978@users.noreply.github.com>
2026-09-18 08:56:37 -0300

    Reverte backend Java, volta para PHP

--- java_backend/src/main/java/com/tcc/backend/repository/MotoristaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tcc.backend.repository;
-
-import com.tcc.backend.model.Motorista;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MotoristaRepository extends JpaRepository<Motorista, Long> {
-}
